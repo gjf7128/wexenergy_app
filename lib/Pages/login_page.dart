03264266236774edd2083486a8e9bc1d3950300f
gjf7128 <gjf7128@rit.edu>
2023-11-01 09:53:35 -0400

    Re-positioned TextField, fixed material app bug

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -3,15 +3,55 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+  static const String welcomeString = 'Log Into WexEnergy Windows App';
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Center(
-          child: Text("howdy"),
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 250,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(welcomeString),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'email',
+                ),
+              ),
+            ],
+          ),
         ),
-        Text("data")
-      ],
+      ),
+    );
+  }
+
+  /// Builds a magic link TextField
+  Widget magicLinkTextField() {
+    return const SizedBox(
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'emailed code',
+        ),
+      ),
+    );
+  }
+
+  /// Builds a password TextField
+  Widget passwordTextField() {
+    return const SizedBox(
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password',
+        ),
+      ),
     );
   }
 }
