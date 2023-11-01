b869736ec26db47ee5524f514b3b69fc1e7fe56c
gjf7128 <gjf7128@rit.edu>
2023-11-01 11:52:35 -0400

    added submit textbutton and padding

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -4,24 +4,51 @@ class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   static const String welcomeString = 'Log Into WexEnergy Windows App';
+  static const String magicLinkLabel = 'Emailed code';
+  static const String passwordLabel = 'Password';
+  static const String usernameOrEmailLabel = 'Email';
+  static const String submitButtonLabel = 'Submit';
+  static const double widgetWidthSize = 250;
+  static const double paddingAllSides = 10;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: SizedBox(
-          width: 250,
+          width: widgetWidthSize,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(welcomeString),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'email',
+              const Text(
+                  welcomeString), //TODO: (gjf7128): input wexEnergy image
+              const Padding(
+                padding: EdgeInsets.all(paddingAllSides),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: usernameOrEmailLabel,
+                  ),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(paddingAllSides),
+                child: passwordTextField(),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(paddingAllSides),
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                      Colors.blue,
+                    ),
+                  ),
+                  onPressed: null, // Transition here
+                  child: Text(submitButtonLabel),
+                ),
+              )
+              // TODO(gjf7128): Clickable Text 'forgot password?
             ],
           ),
         ),
@@ -30,27 +57,25 @@ class LoginPage extends StatelessWidget {
   }
 
   /// Builds a magic link TextField
+  ///
+  /// Use this if we've settled on magic link over password
   Widget magicLinkTextField() {
-    return const SizedBox(
-      width: 250,
-      child: TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'emailed code',
-        ),
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: magicLinkLabel,
       ),
     );
   }
 
-  /// Builds a password TextField
+  /// Builds a password TextField.
+  ///
+  /// Use this if we've settled on password over magic link.
   Widget passwordTextField() {
-    return const SizedBox(
-      width: 250,
-      child: TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Password',
-        ),
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: passwordLabel,
       ),
     );
   }
