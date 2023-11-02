592530d153748d90e696967c4a6d893c125793f0
Ethan Herbst <70246197+ebh1540@users.noreply.github.com>
2023-11-02 11:40:11 -0400

    Got rid of example code

--- lib/Pages/windowspecs_page.dart
+++ lib/Pages/windowspecs_page.dart
@@ -136,54 +136,6 @@ class _ProfileRadioState extends State<ProfileRadio>{
 }
 
 
-
-enum SingingCharacter { lafayette, jefferson }
-
-class RadioExample extends StatefulWidget {
-  const RadioExample({super.key});
-
-  @override
-  State<RadioExample> createState() => _RadioExampleState();
-}
-
-class _RadioExampleState extends State<RadioExample> {
-  SingingCharacter? _character = SingingCharacter.lafayette;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: const Text('Lafayette'),
-          leading: Radio<SingingCharacter>(
-            value: SingingCharacter.lafayette,
-            groupValue: _character,
-            onChanged: (SingingCharacter? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-        ListTile(
-          title: const Text('Thomas Jefferson'),
-          leading: Radio<SingingCharacter>(
-            value: SingingCharacter.jefferson,
-            groupValue: _character,
-            onChanged: (SingingCharacter? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-
-
 class ShapePainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
