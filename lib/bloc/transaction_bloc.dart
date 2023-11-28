5f44a586141671660a543111bdff125ece5ad534
gjf7128 <gjf7128@rit.edu>
2023-11-28 18:06:35 -0500

    implemented core of the Bloc

--- lib/bloc/transaction_bloc.dart
+++ lib/bloc/transaction_bloc.dart
@@ -1,14 +1,19 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:wexenergy_app/bloc/transaction_event.dart';
-// import 'package:wexenergy_app/bloc/transaction_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wexenergy_app/bloc/transaction_event.dart';
+import 'package:wexenergy_app/bloc/transaction_state.dart';
 
-// class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
-//   final TransactionRepository transactionRepository;
-//   TransactionBloc({required this.transactionRepository}) : super(const TransactionState()) {
-//     on<UserLoginEvent>(login);
-//   }
+class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
+  // final TransactionRepository transactionRepository;  TODO: uncomment these 2 lines once repo is set
+  TransactionBloc(/*{required this.transactionRepository}*/)
+      : super(const TransactionState.initialize()) {
+    on<UserLoginEvent>(userLoginHandler);
+  }
 
-//   login(UserLoginEvent event, Emitter<TransactionState> emit) {
-    
-//   }
-// }
+  userLoginHandler(UserLoginEvent event, Emitter<TransactionState> emit) {
+    // Do logic
+    // transactionRepository.login();
+
+    // if login is good
+    // emit(state.loggedIn = true)
+  }
+}
--- lib/bloc/transaction_state.dart
+++ lib/bloc/transaction_state.dart
@@ -3,5 +3,11 @@ import 'package:wexenergy_app/models/customer.dart';
 class TransactionState {
   final Customer customer;
 
-  const TransactionState({required this.customer});
+  final bool loggedIn;
+
+  const TransactionState({required this.customer, required this.loggedIn});
+
+  const TransactionState.initialize()
+      : customer = const Customer.initialize(),
+        loggedIn = false;
 }
